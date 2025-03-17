28948cc3212195f0fd7d4694a04e0f0cd4462be8
WON23Marie <mar.arnold.23@lehre.mosbach.dhbw.de>
2025-03-17 10:34:17 +0100

    Changed: deleted non-functionals plant details in PlantDetails.vue due to API restrictions

--- src/components/PlantDetails.vue
+++ src/components/PlantDetails.vue
@@ -11,44 +11,6 @@
           <p>❗ Keine gängigen Namen gefunden.</p>
         </div>
   
-        <div v-if="plantData.species.description">
-          <p><strong>Beschreibung:</strong> {{ plantData.species.description }}</p>
-        </div>
-  
-        <!-- Bilder -->
-        <div v-if="plantData.images && plantData.images.length" class="image-info">
-          <p><strong>Bildquelle:</strong> <a :href="plantData.images[0].url.o" target="_blank" class="link">Bild anzeigen</a></p>
-          <p><strong>Bildlizenz:</strong> {{ plantData.images[0].license || 'Keine Lizenz verfügbar' }}</p>
-          <p><strong>Bildautor:</strong> {{ plantData.images[0].author || 'Unbekannt' }}</p>
-        </div>
-  
-        <!-- Pflegehinweise -->
-        <div v-if="plantData.species.careTips && plantData.species.careTips.length">
-          <p><strong>Pflegehinweise:</strong></p>
-          <ul>
-            <li v-for="(tip, index) in plantData.species.careTips" :key="index">{{ tip }}</li>
-          </ul>
-        </div>
-        <div v-else>
-          <p>❗ Keine Pflegehinweise verfügbar.</p>
-        </div>
-  
-        <!-- Geografische Herkunft -->
-        <div v-if="plantData.species.geographicArea">
-          <p><strong>Herkunft:</strong> {{ plantData.species.geographicArea }}</p>
-        </div>
-        <div v-else>
-          <p>❗ Keine geografische Herkunft gefunden.</p>
-        </div>
-  
-        <!-- Schutzstatus -->
-        <div v-if="plantData.iucn && plantData.iucn.category">
-          <p><strong>Schutzstatus:</strong> {{ plantData.iucn.category }}</p>
-        </div>
-        <div v-else>
-          <p>❗ Kein Schutzstatus verfügbar.</p>
-        </div>
-  
         <!-- Weitere Taxonomische Daten -->
         <div v-if="plantData.species.family && plantData.species.family.scientificName">
           <p><strong>Familie:</strong> {{ plantData.species.family.scientificName }}</p>
@@ -58,43 +20,17 @@
           <p><strong>Gattung:</strong> {{ plantData.species.genus.scientificName }}</p>
         </div>
   
-        <div v-if="plantData.species.kingdom && plantData.species.kingdom.scientificName">
-          <p><strong>Königreich:</strong> {{ plantData.species.kingdom.scientificName }}</p>
-        </div>
-  
-        <div v-if="plantData.species.order && plantData.species.order.scientificName">
-          <p><strong>Ordnung:</strong> {{ plantData.species.order.scientificName }}</p>
-        </div>
-  
-        <div v-if="plantData.species.phylum && plantData.species.phylum.scientificName">
-          <p><strong>Phylum:</strong> {{ plantData.species.phylum.scientificName }}</p>
-        </div>
-  
-        <div v-if="plantData.species.class && plantData.species.class.scientificName">
-          <p><strong>Klasse:</strong> {{ plantData.species.class.scientificName }}</p>
-        </div>
-  
-        <div v-if="plantData.species.subFamily && plantData.species.subFamily.scientificName">
-          <p><strong>Unterfamilie:</strong> {{ plantData.species.subFamily.scientificName }}</p>
-        </div>
-  
         <!-- GBIF ID -->
         <div v-if="plantData.gbif && plantData.gbif.id">
           <p><strong>GBIF ID:</strong> <a :href="'https://www.gbif.org/species/' + plantData.gbif.id" target="_blank" class="link">Mehr erfahren</a></p>
         </div>
   
-        <div v-if="plantData.species.synonyms && plantData.species.synonyms.length">
-          <p><strong>Synonyme:</strong> {{ plantData.species.synonyms.join(', ') }}</p>
-        </div>
   
-        <div v-if="plantData.species.hybrids && plantData.species.hybrids.length">
-          <p><strong>Hybriden:</strong> {{ plantData.species.hybrids.join(', ') }}</p>
-        </div>
-      </div>
       <div v-else>
         <p>❗ Keine Pflanzendaten verfügbar.</p>
       </div>
     </div>
+  </div>
   </template>
   
   <script>
